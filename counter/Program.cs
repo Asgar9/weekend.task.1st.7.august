b6794e55ec53f71c404596be30079b6ec2b07eb1
Asgar9 <asgaryas@code.edu.az>
2022-08-07 02:59:04 +0400

    counter of digit

---
+++ counter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the digit:");
+            Console.WriteLine(NumLength(int.Parse(Console.ReadLine( ))));
+        }
+        static int NumLength(int number)
+        {
+           int test = number;
+            int count = 0;
+            while (test>0)
+            {
+                test = test / 10;
+                count++;
+            }
+            return count;
+        }
+    }
+}
